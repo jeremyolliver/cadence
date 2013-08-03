6ae5fc3e946f14d868f4f2af3e7b9c77ecd61f4a
Jeremy Olliver <jeremy.olliver@gmail.com>
2013-08-03 16:07:55 +1200

    New rails project
    - test unit skipped
    - configured for postgres AR
    - gitignore adjusted

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cadence::Application.config.secret_key_base = 'fc9a887250d47f71433c9a7e94e72fd96c6746fc256e178a61e1f46323dc90df292348147c7b6e3447765e56927da3cac4d897a5e6bec7121c9aecd0acbb299a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cadence::Application.config.session_store :cookie_store, key: '_cadence_session'
